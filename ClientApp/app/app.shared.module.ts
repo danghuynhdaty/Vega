a5440a40a31dd152a8cf8d587460fc46dc1f32f0
daty.danghuynh <daty.danghuynh20497@gmail.com>
2018-07-24 12:44:11 +0700

    create make service

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -1,34 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { MakeService } from "./services/make.service";
+import { VehicleFormComponent } from "./components/vehicle-form/vehicle-form.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { AppComponent } from "./components/app/app.component";
+import { NavMenuComponent } from "./components/navmenu/navmenu.component";
+import { HomeComponent } from "./components/home/home.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        VehicleFormComponent
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: "", redirectTo: "home", pathMatch: "full" },
+            { path: "home", component: HomeComponent },
+            { path: "vehicles/new", component: VehicleFormComponent },
+            { path: "**", redirectTo: "home" }
         ])
+    ],
+    providers: [
+        MakeService
     ]
 })
 export class AppModuleShared {
