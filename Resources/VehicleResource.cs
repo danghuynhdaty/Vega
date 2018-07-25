2e83b4fe97256618aab6126c5b363bf9c6ff7e4c
daty.danghuynh <daty.danghuynh20497@gmail.com>
2018-07-26 00:52:39 +0700

    Add ContactResource and VehicleResource

---
+++ Resources/VehicleResource.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Vega.Resources
+{
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public bool IsRegistered { get; set; }
+        public ICollection<int> VehicleFeatures { get; set; }
+
+        public VehicleResource()
+        {
+            VehicleFeatures = new Collection<int>();
+        }
+    }
+}
